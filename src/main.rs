2582ae1bb324b048abc0d2f46b096e5467d88f0f
Richard Howard <richard@howard.io>
2018-07-19 11:38:27 -0400

    Fetch PRs from GitHub and parse response

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,16 @@
 extern crate clap;
-extern crate reqwest;
 
 #[macro_use]
 extern crate serde_derive;
 
 mod config;
 mod types;
+mod github;
 
-use reqwest::Url;
 use clap::{App, Arg};
 use config::read_config_file;
-use types::Repo;
+use github::fetch_prs;
 
-fn fetch_repo(repo: &Repo) -> Result<String, reqwest::Error> {
-    let url = format!(
-        "https://api.github.com/repos/{}/{}/pulls",
-        repo.owner, repo.name
-    );
-    let uri = Url::parse(&url).expect("Could not parse url.");
-    let response_text = reqwest::get(uri)
-        .expect("Request failed.")
-        .text()
-        .expect("Could not get json");
-    Ok(response_text)
-}
 
 fn main() {
     let matches = App::new("pears")
@@ -42,7 +29,6 @@ fn main() {
     let config = read_config_file(matches.value_of("config").unwrap())
         .expect("Could not parse config file.");
 
-    let repo = &config.repos[0];
-    let r = fetch_repo(repo);
-    println!("{}", r.unwrap());
+    let repo = &config.repos[2];
+    let prs = fetch_prs(repo).expect("Could not reach GitHub API.");
 }
--- src/types.rs
+++ src/types.rs
@@ -1,12 +1,48 @@
-#[derive(Deserialize)]
-pub struct Repo {
+#[derive(Deserialize, Debug)]
+pub struct ConfigRepo {
     pub owner: String,
     pub name: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Config {
     pub me: String,
     pub token: String,
-    pub repos: Vec<Repo>,
+    pub repos: Vec<ConfigRepo>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct GitHubRepo {
+    pub owner: String,
+    pub name: String,
+    pub id: i32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct GitHubUser {
+    pub login: String,
+    pub id: i32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct GitHubCommit {
+    pub sha: String,
+    pub user: GitHubUser,
+    pub repo: GitHubRepo,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct GitHubPullRequest {
+    pub url: String,
+    pub id: i32,
+    pub state: String,
+    pub title: String,
+    pub body: Option<String>,
+    pub number: i32,
+    pub created_at: String,
+    pub updated_at: String,
+    pub closed_at: Option<String>,
+    pub merged_at: Option<String>,
+
+    pub user: GitHubUser
 }
