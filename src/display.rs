4d7d9b59ae1ccda3d623d434ee62fa3ebe4cc633
Richard Howard <richard@howard.io>
2018-09-26 17:07:45 -0400

    Let's format this Duration ourselves

--- src/display.rs
+++ src/display.rs
@@ -1,16 +1,41 @@
 use types;
 
 use console::{Color, Style, Term, Attribute};
+use chrono::prelude::*;
 
 pub struct PearsDisplay {
     term: Term,
     width: usize,
 }
 
-// fn ago(timestamp_str: String) {
-//     let timestamp = DateTime::parse_from_str(timestamp_str);
-//     let now = Local::now();
-// }
+fn ago(timestamp: DateTime<Utc>) -> String {
+    let now = Utc::now();
+    let duration = now - timestamp;
+
+    let days = duration.num_days();
+    let hours = duration.num_hours();
+    let minutes = duration.num_minutes();
+    let seconds = duration.num_seconds();
+
+    let noun: &str;
+    let count: i64;
+
+    if days > 0 {
+        count = days;
+        noun = if days > 1 { "days" } else { "day" };
+    } else if hours > 0 {
+        count = hours;
+        noun = if days > 1 { "hours" } else { "hour" };
+    } else if minutes > 0 {
+        count = minutes;
+        noun = if days > 1 { "minutes" } else { "minute" };
+    } else {
+        count = seconds;
+        noun = if days > 1 { "seconds" } else { "second" };
+    }
+
+    format!("{} {}", count, noun)
+}
 
 impl PearsDisplay {
     pub fn new() -> PearsDisplay {
@@ -35,9 +60,10 @@ impl PearsDisplay {
 
     pub fn pr(&self, pr: types::GitHubPullRequest) {
         let url_style = Style::new().attr(Attribute::Dim);
+
         let line = format!(
-            "   [#{}] {}\n   Last updated {}\n   {}\n",
-            pr.number, pr.title, pr.updated_at, url_style.apply_to(pr.url)
+            "   [#{}] {}\n   Updated {} ago\n   {}\n",
+            pr.number, pr.title, ago(pr.updated_at), url_style.apply_to(pr.url)
         );
         self.term.write_line(line.as_str()).unwrap();
     }
